914a2d20194e7cc563c2ac13139bade42f411a27
davis <davisochieng4@gmail.com>
2023-05-08 08:52:11 -0400

    Add Recursion )

--- index.js
+++ index.js
@@ -7,5 +7,15 @@ import {
 	contUniquevalues2,
 	maxSubArraySum,
 } from './src/index.js';
+import {
+	coutDown,
+	someRange,
+	factorial,
+	recursiveFactorial,
+} from './src/recusion.js';
 
-console.log(maxSubArraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+// console.log(maxSubArraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+
+// coutDown(10);
+// console.log(someRange(4));
+console.log('Factorial: ', recursiveFactorial(5));
---
+++ src/recusion.js
@@ -0,0 +1,34 @@
+export function coutDown(num) {
+	if (num <= 0) {
+		console.log('All done');
+		return;
+	}
+	console.log(num);
+	num--;
+	coutDown(num);
+}
+
+export function someRange(num) {
+	if (num === 1) {
+		return 1;
+	} else {
+		console.log(` num => ${num}`);
+		return num + someRange(num - 1);
+	}
+}
+
+export function factorial(num) {
+	let result = 0;
+	for (let i = num; i > 0; num--) {
+		result = result * i;
+	}
+	return result;
+}
+
+export function recursiveFactorial(num) {
+	if (num === 1) {
+		return i;
+	} else {
+		return num * factorial(num - 1);
+	}
+}
